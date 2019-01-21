2290b5c4d5517177000bc5e4828a435c43ae296a
Winnerawan T <third1337@gmail.com>
2019-01-21 19:02:12 +0700

    update

--- app/Http/Controllers/API/AppController.php
+++ app/Http/Controllers/API/AppController.php
@@ -12,7 +12,7 @@ class AppController extends Controller
         $app = [
             "banner" => "ca-app-pub-9400864410179150/3066383366",
             "inters" => "ca-app-pub-9400864410179150/7587425763",
-            "reward" => "rewards",
+            "reward" => "ca-app-pub-9400864410179150/1281995578",
             "safe_mode" => false
         ];
 
