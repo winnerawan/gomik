cfc8aacf9a4132a6e07331ef61ab1594b7420cfc
Winnerawan T <third1337@gmail.com>
2019-01-23 14:13:50 +0700

    update

--- app/Http/Controllers/API/AppController.php
+++ app/Http/Controllers/API/AppController.php
@@ -10,8 +10,8 @@ class AppController extends Controller
     public function app() {
 
         $app = [
-            "banner" => "ca-app-pub-9400864410179150/9729777567",
-            "inters" => "ca-app-pub-9400864410179150/5946568939",
+            "banner" => "ca-app-pub-9400864410179150/3066383366",
+            "inters" => "ca-app-pub-9400864410179150/7587425763",
             "reward" => "ca-app-pub-9400864410179150/1281995578",
             "safe_mode" => false
         ];
